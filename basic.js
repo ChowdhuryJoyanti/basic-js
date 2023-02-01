49f8d7fef4b3a1c2829caff0f08b4c9ccf6b5ee4
Joyantichy <120155482+Joyantichy@users.noreply.github.com>
2023-02-01 10:04:50 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,26 @@
+var num1 = 600;
+var num2 = 800;
+var num3 = 300;
+
+if(num1 > num2 && num1 > num3 ){
+    console.log('number 1 theke sob theke boro');
+}
+else if (num2 > num3){
+    console.log('number 2 sob theke boro');
+}
+else{
+    console.log('number 3 sob theke boro');
+}
+var num5 = 100;
+
+if (num5 > 100){
+    console.log("this is bigger than 100");
+  
+}
+else if (num5  < 100){
+    console.log('this is smaller than 100');
+
+}
+else{
+    console.log('This is equal to hundread');
+}
